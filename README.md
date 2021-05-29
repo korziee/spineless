f3c5502194b725dacf29f2f4c551c1c94a5241a7
Jye Lewis <jye@jyelewis.com>
2021-05-30 09:41:27 +1000

    Init Jekyll

--- README.md
+++
@@ -1,6 +0,0 @@
-Using template:
-
-- change reset.sh values to suit project/db name
-- change package.json values to suit project name
-- add database migration file for tables
-- update/add seeds
--- packages/server/src/modules/healthCheck/HealthCheckModule.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { HealthCheckController } from "./HealthCheckController";
-
-@Module({
-  imports: [],
-  controllers: [HealthCheckController],
-})
-export class HealthCheckModule {}
