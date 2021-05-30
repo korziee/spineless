d113d3a993e88323044398b4e874104e1ccdddb7
Kory Porter <kory.porter@hotmail.com>
2021-05-30 11:34:59 +1000

    add infra to top level

--- spineless-infra/bin/infra.ts
+++ spineless-infra/bin/infra.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
-import { GardenStack } from "../lib/garden-stack";
+import { SpinelessStack } from "../lib/garden-stack";
 
 const app = new cdk.App();
 
 if (process.env.ENVIRONMENT === "dev") {
-  new GardenStack(app, "dev", {
+  new SpinelessStack(app, "dev", {
     /* If you don't specify 'env', this stack will be environment-agnostic.
      * Account/Region-dependent features and context lookups will not work,
      * but a single synthesized template can be deployed anywhere. */
@@ -17,7 +17,7 @@ if (process.env.ENVIRONMENT === "dev") {
 
     /* Uncomment the next line if you know exactly what Account and Region you
      * want to deploy the stack to. */
-    env: { account: "970210967831", region: "ap-southeast-2" },
+    env: { account: "462901810174", region: "ap-southeast-2" },
 
     /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
   });
