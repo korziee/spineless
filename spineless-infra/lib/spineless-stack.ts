91e0ad111951a6ee7b92fbbcd77384f979016ed3
Kory Porter <kory.porter@hotmail.com>
2021-05-30 15:53:56 +1000

    add infra with cloudfront

--- spineless-infra/lib/spineless-stack.ts
+++ spineless-infra/lib/spineless-stack.ts
@@ -4,76 +4,34 @@ import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as route53 from "@aws-cdk/aws-route53";
 import * as cdk from "@aws-cdk/core";
-import * as origins from "@aws-cdk/aws-cloudfront-origins";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as route53Targets from "@aws-cdk/aws-route53-targets";
 
 import * as path from "path";
+
+const SPINELESS_CERT_ARN =
+  "arn:aws:acm:us-east-1:462901810174:certificate/79ee09a8-a072-46b2-800b-67d30197fcb9";
 
 export class SpinelessStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const zone = route53.HostedZone.fromHostedZoneAttributes(
+    const hostedZone = route53.HostedZone.fromLookup(
       this,
       "spineless-hosted-zone",
       {
-        hostedZoneId: "Z03819212MSQFSXR2027E",
-        zoneName: "spineless.xyz",
+        domainName: "spineless.xyz",
       }
     );
 
-    // create TLS certificate
-    const myCertificate = new acm.DnsValidatedCertificate(
+    const certificate = acm.Certificate.fromCertificateArn(
       this,
       "spineless-certificate",
-      {
-        domainName: "spineless.xyz",
-        hostedZone: zone,
-        region: "us-east-1",
-      }
+      SPINELESS_CERT_ARN
     );
 
-    const backendFn = new lambda.Function(this, "spineless-lambda", {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      handler: "index.handler",
-      // TODO: Replace with directory with compiled app
-      code: lambda.Code.fromAsset(path.join(__dirname, "../lambda_temp")),
-    });
-
-    const api = new apigateway.LambdaRestApi(this, "spineless-api", {
-      handler: backendFn,
-    });
-
-    const websiteS3 = new s3.Bucket(this, "spineless-website");
-
-    const websiteOrigin = new origins.S3Origin(websiteS3);
-
-    const cfDistribution = new cloudfront.Distribution(this, "myDist", {
-      defaultBehavior: { origin: new origins.HttpOrigin(api.url) },
-      additionalBehaviors: {
-        // TODO better way to point additional behaviors?
-        "/": {
-          origin: websiteOrigin,
-        },
-        "/static/*": {
-          origin: websiteOrigin,
-        },
-        "/editor/*": {
-          origin: websiteOrigin,
-        },
-      },
-      domainNames: ["spineless.xyz"],
-      certificate: myCertificate,
-    });
-
-    // we need this information to assume credentials locally
-    new cdk.CfnOutput(this, "Cloudfront Distro URL", {
-      value: cfDistribution.distributionDomainName,
-    });
-
     const dataTable = new dynamodb.Table(this, "spineless_data", {
-      // namespace/table
       partitionKey: {
         name: "namespaced_table",
         type: dynamodb.AttributeType.STRING,
@@ -87,6 +45,93 @@ export class SpinelessStack extends cdk.Stack {
         type: dynamodb.AttributeType.STRING,
       },
       sortKey: { name: "table", type: dynamodb.AttributeType.STRING },
+    });
+
+    const nodeModulesLayer = new lambda.LayerVersion(
+      this,
+      "spineless-modules",
+      {
+        code: lambda.Code.fromAsset(
+          path.join(__dirname, "../../spineless-server/node_modules")
+        ),
+        compatibleRuntimes: [lambda.Runtime.NODEJS_14_X],
+      }
+    );
+
+    const backendLambda = new lambda.Function(this, "spineless-lambda", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, "../../spineless-server/dist")
+      ),
+      handler: "api.handler",
+      layers: [nodeModulesLayer],
+      environment: {
+        DB_SPINELESS_TABLES: metaTable.tableArn,
+        DB_SPINELESS_DATA: dataTable.tableArn,
+      },
+    });
+
+    const api = new apigateway.LambdaRestApi(this, "spineless-api", {
+      handler: backendLambda,
+    });
+
+    const websiteS3 = new s3.Bucket(this, "spineless-website");
+
+    const cloudfrontDistribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "MyDistribution",
+      {
+        comment: "CDN for Web App",
+        viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(
+          certificate,
+          {
+            aliases: ["spineless.xyz"],
+          }
+        ),
+        originConfigs: [
+          {
+            // make sure your backend origin is first in the originConfigs list so it takes precedence over the S3 origin
+            customOriginSource: {
+              domainName: `${api.restApiId}.execute-api.${this.region}.${this.urlSuffix}`,
+            },
+            originPath: `/${api.deploymentStage.stageName}`,
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+                cachedMethods: undefined,
+                allowedMethods: cloudfront.CloudFrontAllowedMethods.ALL,
+              },
+            ],
+          },
+          {
+            s3OriginSource: {
+              s3BucketSource: websiteS3,
+            },
+            behaviors: [
+              {
+                pathPattern: "/",
+              },
+              {
+                pathPattern: "/static/*",
+              },
+              {
+                pathPattern: "/editor/*",
+              },
+            ],
+          },
+        ],
+      }
+    );
+
+    new route53.ARecord(this, "AliasRecord", {
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.CloudFrontTarget(cloudfrontDistribution)
+      ),
+    });
+
+    new cdk.CfnOutput(this, "myOut", {
+      value: cloudfrontDistribution.distributionDomainName,
     });
   }
 }
