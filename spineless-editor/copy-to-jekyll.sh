32f583e2c380e7118c88e9929f2648d5ed8cc193
Jye Lewis <jye@jyelewis.com>
2021-08-20 14:03:17 +1000

    Comments explaining copy-to-jekyll.sh

--- spineless-editor/copy-to-jekyll.sh
+++ spineless-editor/copy-to-jekyll.sh
@@ -4,6 +4,11 @@
 rm -rf ../spineless-website/assets/editor/
 cp -R build/ ../spineless-website/assets/editor/
 
+# CRA wants control over the output index.html file
+# however, we want our app to load inside a custom jekyll template editor.html file
+# to avoid ejecting from CRA, let CRA generate it's index.html file,
+# then strip out the basic html tags, and copy the resulting page into our editor.template.html file
+
 # reset editor.html entrypoint file
 cp editor.template.html ../spineless-website/editor.html
 # copy in CRA index.html file into editor.html
