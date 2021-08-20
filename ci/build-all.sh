82c37a306bbd7b470c80e51668a63aba93529ada
Jye Lewis <jye@jyelewis.com>
2021-08-20 16:03:42 +1000

    Include spineless-editor in build script

--- ci/build-all.sh
+++ ci/build-all.sh
@@ -8,6 +8,12 @@ rm -rf node_modules
 yarn --production=true
 cd ../
 
+# build spineless-editor
+cd ../spineless-editor
+yarn install
+yarn build-and-copy
+cd ../
+
 # build spineless-website
 cd spineless-website
 bundle install
